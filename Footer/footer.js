ad6f0764369e584d16237adf86bce5ad22e06546
Vishaltandale987 <tandalevishal987@gmail.com>
2022-08-24 21:40:33 +0530

    footer and login page

---
+++ Footer/footer.js
@@ -0,0 +1,78 @@
+function footer() {
+    return `
+    <div id="top">
+    <div id="customer_care">
+        <h5>CUSTOMER CARE</h5>
+        <p>Shopper Protection</p>
+        <p>Loyalty Program</p>
+        <p>Help Center</p>
+        <p>Size Guides</p>
+        <p>Contact Us / Feedback</p>
+        <p>Contact Us / Feedback</p>
+    </div>
+
+    <div id="information">
+        <h5>INFORMATION</h5>
+        <p>About Us</p>
+        <p>Influencer Program</p>
+        <p>Partner Stores</p>
+        <p>Sitemap</p>
+        
+    </div>
+
+    <div id="legal">
+        <h5>LEGAL</h5>
+        <p>Terms Of Use</p>
+        <p>Disclosure</p>
+        <p>Privacy Policy</p>
+        <p>Community Guidelines</p>
+        
+    </div>
+
+    <div id="connect">
+        <h5>CONNECT WITH US</h5>
+        <div class="img">
+            <img src="https://cdn.modesens.com/static/img/20220815download_ios.svg" alt="">
+            <img src="https://cdn.modesens.com/static/img/20220815download_android.svg" alt="">
+            <img src="https://cdn.modesens.com/static/img/20220815down_chromestrore.svg" alt="">
+        </div>
+    </div>
+
+    <div id="sign_up">
+        <h5>NEWSLETTER SIGN UP</h5>
+        <div>
+            <input type="text" id="email" placeholder="Email Address">
+            <div class="select">
+            <input type="checkbox" id="click">
+                <p>Women</p>
+            <input type="checkbox" id="click">
+
+                <p>Man</p>
+            </div>
+            <button>SUBMIT</button>
+        </div> 
+       
+    </div>
+
+</div>
+
+<div id="bottom">
+    <img src="https://cdn.modesens.com/static/img/20180905footer_logo.svg" alt="">
+    <p>© 2022, ModeSens Inc</p>
+    
+
+        <img src="https://cdn.modesens.com/static/img/20220726Pinterest.svg" class="logo" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220726Twitter.svg" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220726Ins.svg" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220726Facebook.svg" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220726Weibo.svg" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220726Wechat.svg" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220726Reddit.svg" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220726Discord.svg" alt="">
+        <img src="https://cdn.modesens.com/static/img/20220811Xiaohongshu.svg" alt="">
+    
+        
+</div>`;
+}
+
+export default footer();
